5c2472afe7747219f047c422944a15362561492d
Claudio Godoy <clouds83@gmail.com>
2023-03-15 15:49:07 -0300

    updated

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.clouds.handychef',
+  appName: 'HandyChef',
+  webDir: 'www',
+  bundledWebRuntime: false,
+};
+
+export default config;
